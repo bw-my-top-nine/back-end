2c723bc7193f1f6289f2018fb5b7251ae8109849
Zach Lasky <zacharylasky123@gmail.com>
2019-09-23 19:38:23 -0400

    updated port env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const server = require('./api/server.js');
 
-const PORT = process.env.port || 5678
+const PORT = process.env.PORT || 5678
 
 server.listen(PORT, () => {
   console.log(`API Running on port ${PORT}`)
