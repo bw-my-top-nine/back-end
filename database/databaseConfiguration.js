ae497df2c1582a615184784663be04d298fdda62
Zach Lasky <zacharylasky123@gmail.com>
2019-09-23 20:25:42 -0400

    added db_env

--- database/databaseConfiguration.js
+++ database/databaseConfiguration.js
@@ -2,4 +2,6 @@ const knex = require('knex')
 
 const knexConfiguration = require('../knexfile.js')
 
+const dbENV = process.env.DB_ENV || 'development'
+
 module.exports = knex(knexConfiguration.development)
