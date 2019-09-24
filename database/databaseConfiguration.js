82b3c5d50590e47e0c1837ecee157eb058ec7f2d
Zach Lasky <zacharylasky123@gmail.com>
2019-09-23 22:17:32 -0400

    knex changes

--- database/databaseConfiguration.js
+++ database/databaseConfiguration.js
@@ -2,6 +2,4 @@ const knex = require('knex')
 
 const knexConfiguration = require('../knexfile.js')
 
-const environment = process.env.DB_ENV || 'development'
-
-module.exports = knex(knexConfiguration[environment])
+module.exports = knex(knexConfiguration.development)
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5678
 
 server.listen(port, () => {
   console.log(`API Running on port ${port}`)
