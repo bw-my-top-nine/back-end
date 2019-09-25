214c8a1b5de9a7461b2618b38d00d347730df303
Zach Lasky <zacharylasky123@gmail.com>
2019-09-25 18:59:34 -0400

    auth and categories testing added

---
+++ authentication/authenticationRouter.spec.js
@@ -0,0 +1,55 @@
+const request = require("supertest")
+const server = require("../api/server.js")
+
+// describe('router.js', () => {
+//   describe('POST /register', () => {
+//     let user = {
+//       email: "user9@gmail.com",
+//       password: "password"
+//     }
+//     it('returns 201 created & toMatch json', () => {
+//       return request(server)
+//         .post('/api/authentication/register')
+//         .send(user)
+//         .then(response => {
+//           expect(response.status).toBe(201);
+//           expect(response.type).toMatch(/json/i)
+//         });
+//     });
+//   });
+// });
+
+describe('authenticationRouter.js', () => {
+  describe('POST /login', () => {
+    let user = {
+      email: "user7@gmail.com",
+      password: "password"
+    }
+    it('returns 200 ok & toMatch json', () => {
+      return request(server)
+        .post('/api/authentication/login')
+        .send(user)
+        .then(response => {
+          expect(response.status).toBe(200);
+          expect(response.type).toMatch(/json/i)
+        });
+    });
+  });
+});
+
+describe('authenticationRouter.js', () => {
+  describe('DELETE /delete', () => {
+    let user = {
+      email: "user7@gmail.com",
+      password: "password"
+    }
+    it('returns 200 ok & toMatch json', () => {
+      return request(server)
+        .delete('/api/authentication/users/1')
+        .send(user)
+        .then(response => {
+          expect(response.status).toBe(200);
+        });
+    });
+  });
+});
