8a3d580aa9356541d0c0af4e04a6c176425c4f11
Zach Lasky <zacharylasky123@gmail.com>
2019-09-25 19:47:24 -0400

    changes

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -1,15 +1,17 @@
+const bcrypt = require("bcryptjs");
+
 exports.seed = function(knex) {
   return knex('users').insert([
     
-    { email: "user1@gmail.com", password: "password1" },
+    { email: "user1@gmail.com", password: bcrypt.hashSync("password1") },
     
-    { email: "user2@gmail.com", password: "password2" }, 
+    { email: "user2@gmail.com", password: bcrypt.hashSync("password2") }, 
     
-    { email: "user3@gmail.com", password: "password3" }, 
+    { email: "user3@gmail.com", password: bcrypt.hashSync("password3") }, 
     
-    { email: "user4@gmail.com", password: "password4" }, 
+    { email: "user4@gmail.com", password: bcrypt.hashSync("password4") }, 
     
-    { email: "user5@gmail.com", password: "password5" }
+    { email: "user5@gmail.com", password: bcrypt.hashSync("password5") }
   
   ]);
 };
