a525ac7306421c9d1a15e0c306080ff96e906690
Zach Lasky <zacharylasky123@gmail.com>
2019-09-25 16:33:56 -0400

    added production pg

--- database/databaseConfiguration.js
+++ database/databaseConfiguration.js
@@ -2,4 +2,6 @@ const knex = require('knex')
 
 const knexConfiguration = require('../knexfile.js')
 
-module.exports = knex(knexConfiguration.development)
+const dbEnv = process.env.DB_ENV || 'development'
+
+module.exports = knex(knexConfiguration[dbEnv])
